08fe37a5869ea2304b13f0cd52b76c56fd421d5d
Anahi Garnelo <agarnelo@Anahis-MacBook.local>
2016-07-19 21:43:57 -0400

    Postgres database setup

--- interroBang/interroBang/settings.py
+++ interroBang/interroBang/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'interroBang.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'interrobang',
+        'USER': 'interrouser',
+        'PASSWORD': 'bangbangbang',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

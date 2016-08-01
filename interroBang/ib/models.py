98b6eea6d62f6887237e76f31097fed96b47d729
Anahi Garnelo <agarnelo@Anahis-MacBook.local>
2016-08-01 18:04:16 -0400

    Fixed some errors in Models.py

--- interroBang/ib/models.py
+++ interroBang/ib/models.py
@@ -49,24 +49,26 @@ class Comment(models.Model):
     creator = models.CharField(max_length=40, null=False) #username
     #cEvent = models.ForeignKey(Event)
     
-class Event(models.Model):
-    text = models.CharField(max_lenght=500)
-    date = models.DateTimeField()
-    creator = models.CharField(max_length=35)
-    public = models.BoolenField()
-    album = models.ForeignKey(Trip, on_delete = models.CASCADE)
-    #location = models.ForeignKey(Location, on_delete = models.CASCADE, blank = true)
+class Dest(models.Model):
+    name = models.CharField(max_length=50)
+    info = models.CharField(max_length=250)
+
 
 class Trip(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
     creator = models.CharField(max_length=35)
-    dests = models.ManyToMany(Dest, on_delete = models.CASCADE)
+    dests = models.ManyToManyField(Dest, on_delete = models.CASCADE)
     #travlers = models.ManyToMany(User, on_delete = models.CASCADE)
 
-class Dest(models.Model):
-    name = models.CharField(max_length=50)
-    info = models.CharField(max_length=250)
+class Event(models.Model):
+    text = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    creator = models.CharField(max_length=35)
+    public = models.BooleanField()
+    album = models.ForeignKey(Trip, on_delete = models.CASCADE)
+    #location = models.ForeignKey(Location, on_delete = models.CASCADE, blank = true)
+
 
 class Note(models.Model):
     content = models.CharField(max_length=250)

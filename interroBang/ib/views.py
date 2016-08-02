7f6ddfbf0b71913bad82bdc99f4106c81cb7d2f7
Anahi Garnelo <agarnelo@Anahis-MacBook.local>
2016-08-02 00:50:56 -0400

    More changes

--- interroBang/ib/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def signup(request):
-    #Here we will get the signup info from the html page
-    return render(request,"index.html", {})
--- interroBang/interroBang/urls.py
+++ interroBang/interroBang/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^signup/', include('ib.urls')),
+    url(r'^admin/', admin.site.urls)
 ]

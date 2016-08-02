04652bd23dbc6d7659fa11d81396f0607a1be123
Anahi Garnelo <agarnelo@Anahis-MacBook.local>
2016-08-01 22:24:54 -0400

    Models ordering changed

--- interroBang/ib/models.py
+++ interroBang/ib/models.py
@@ -17,6 +17,41 @@ class Friend(models.Model):
     #friends list
     fDate = models.DateField(auto_now_add=True)
 
+
+
+    
+class Dest(models.Model):
+    name = models.CharField(max_length=50)
+    info = models.CharField(max_length=250)
+
+class Trip(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    creator = models.CharField(max_length=35)
+    dests = models.ManyToManyField(Dest, on_delete = models.CASCADE)
+    travelers = models.ManyToManyField(User, on_delete = models.CASCADE)
+
+class Event(models.Model):
+    text = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    creator = models.CharField(max_length=35)
+    public = models.BooleanField()
+    album = models.ForeignKey(Trip, on_delete = models.CASCADE)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE, blank = True)
+
+class Comment(models.Model):
+    cID = models.AutoField(primary_key=True)
+    caption = models.CharField(max_length=250, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    creator = models.CharField(max_length=40, null=False) #username
+    cEvent = models.ForeignKey(Event)
+
+class Photo(models.Model):
+    #https://coderwall.com/p/bz0sng/simple-django-image-upload-to-model-imagefield
+    imagePath = models.ImageField(upload_to="/photos", default="photo/none/noimg.jpg")
+    iEvent = models.ForeignKey(Event)
+
+
 class Location(models.Model):
     lName = models.CharField(max_length=50, null=False)
     restaurant = 1
@@ -33,37 +68,6 @@ class Location(models.Model):
     )
     lType = models.IntegerField(choices=lType_choices, null=False, blank=True, default=4)
     city = models.ForeignKey(Dest, on_delete = models.CASCADE)
-
-class Photo(models.Model):
-    #https://coderwall.com/p/bz0sng/simple-django-image-upload-to-model-imagefield
-    imagePath = models.ImageField(upload_to="/photos", default="photo/none/noimg.jpg")
-    iEvent = models.ForeignKey(Event)
-
-class Comment(models.Model):
-    cID = models.AutoField(primary_key=True)
-    caption = models.CharField(max_length=250, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    creator = models.CharField(max_length=40, null=False) #username
-    cEvent = models.ForeignKey(Event)
-    
-class Dest(models.Model):
-    name = models.CharField(max_length=50)
-    info = models.CharField(max_length=250)
-
-class Trip(models.Model):
-    start = models.DateTimeField()
-    end = models.DateTimeField()
-    creator = models.CharField(max_length=35)
-    dests = models.ManyToManyField(Dest, on_delete = models.CASCADE)
-    travelers = models.ManyToMany(User, on_delete = models.CASCADE)
-
-class Event(models.Model):
-    text = models.CharField(max_length=500)
-    date = models.DateTimeField()
-    creator = models.CharField(max_length=35)
-    public = models.BooleanField()
-    album = models.ForeignKey(Trip, on_delete = models.CASCADE)
-    location = models.ForeignKey(Location, on_delete = models.CASCADE, blank = True)
 
 
 class Note(models.Model):

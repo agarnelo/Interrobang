96842d2abdaf27ba9154eb8a49cec0ae13a74af0
Rebeca Otero <rebeca.otero2012@gmail.com>
2016-08-01 22:09:04 -0400

    Db updates

--- interroBang/ib/models.py
+++ interroBang/ib/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    userID = models.AutoField(primary_key=True, null=False)
     username = models.CharField(max_length=40, null=False, unique=True)
     #    passwordHash =
     #(Note: pip install bcrypt in webenv)
@@ -15,12 +14,10 @@ class User(models.Model):
     registration = models.DateField(auto_now_add=True)
 
 class Friend(models.Model):
-    fID = models.AutoField(primary_key=True)
     #friends list
     fDate = models.DateField(auto_now_add=True)
 
 class Location(models.Model):
-    lID = models.AutoField(primary_key=True)
     lName = models.CharField(max_length=50, null=False)
     restaurant = 1
     museum = 2
@@ -35,31 +32,30 @@ class Location(models.Model):
         (4, 'leisure'),
     )
     lType = models.IntegerField(choices=lType_choices, null=False, blank=True, default=4)
+    city = models.ForeignKey(Dest, on_delete = models.CASCADE)
 
 class Photo(models.Model):
-    pID = models.AutoField(primary_key=True)
     #https://coderwall.com/p/bz0sng/simple-django-image-upload-to-model-imagefield
     imagePath = models.ImageField(upload_to="/photos", default="photo/none/noimg.jpg")
-    #iEvent = models.ForeignKey(Event)
+    iEvent = models.ForeignKey(Event)
 
 class Comment(models.Model):
     cID = models.AutoField(primary_key=True)
     caption = models.CharField(max_length=250, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     creator = models.CharField(max_length=40, null=False) #username
-    #cEvent = models.ForeignKey(Event)
+    cEvent = models.ForeignKey(Event)
     
 class Dest(models.Model):
     name = models.CharField(max_length=50)
     info = models.CharField(max_length=250)
-
 
 class Trip(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
     creator = models.CharField(max_length=35)
     dests = models.ManyToManyField(Dest, on_delete = models.CASCADE)
-    #travlers = models.ManyToMany(User, on_delete = models.CASCADE)
+    travelers = models.ManyToMany(User, on_delete = models.CASCADE)
 
 class Event(models.Model):
     text = models.CharField(max_length=500)
@@ -67,12 +63,20 @@ class Event(models.Model):
     creator = models.CharField(max_length=35)
     public = models.BooleanField()
     album = models.ForeignKey(Trip, on_delete = models.CASCADE)
-    #location = models.ForeignKey(Location, on_delete = models.CASCADE, blank = true)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE, blank = True)
 
 
 class Note(models.Model):
     content = models.CharField(max_length=250)
-    type = models.CharField(max_length=10)
-    #reciever = models.ForeignKey(User, on_delete = models.CASCADE, blank = false)
+    site_alert = 0
+    trip_add = 1
+    new_friend = 2
+    new_comment = 3
+    nTypes = ((0, 'site_alert'),
+              (1, 'trip_add'),
+              (2, 'new_friend'),
+              (3, 'new_comment'))
+    type = models.IntegerField(choices=nTypes, null=False, blank=True, default=0)
+    receiver = models.ForeignKey(User, on_delete = models.CASCADE, blank = False)
 
 #http://tutorial.djangogirls.org/en/django_forms/

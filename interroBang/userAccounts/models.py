f45e542a848e7be3def4d0f15ea578f373f4dc94
Anahi Garnelo <garnelo.anahi@gmail.com>
2016-08-17 16:08:25 -0400

    Setting up events app

--- interroBang/userAccounts/models.py
+++ interroBang/userAccounts/models.py
@@ -1,21 +1,6 @@
-# from django.db import models
-# from django.conf import settings
-# from django.db.models.signals import post_save
-# from django.contrib.auth.models import User
-# from django.utils import timezone
-#
-#
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     first_name = models.CharField(max_length=35)
-#     last_name = models.CharField(max_length=35)
-#     def __unicode__(self):
-#         return self.user.username
-#
-
-
 from django.contrib.auth.models import User
 from django.db import models
+
 class Profile(models.Model):
     user = models.OneToOneField(User, unique=True,related_name="profile",  on_delete=models.CASCADE)
     first_name = models.CharField(max_length=35)
